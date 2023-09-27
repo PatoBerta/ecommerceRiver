b431a681e67482ed8543896b77993f4baa50d280
Patricio Berteuris <berteurispatricio@gmail.com>
2023-09-27 11:18:00 -0300

    se agrega el proyecto

---
+++ products.js
@@ -0,0 +1,42 @@
+const shopContent = document.getElementById("shopContent")
+
+const productos = [
+    {
+        id: 1,
+        nombre: "Camiseta Titular",
+        precio: 17500,
+        img:
+            "https://i.pinimg.com/originals/3c/61/7b/3c617bd15f8d5746e387a96065e2d311.jpg",
+            cantidad: 1,  
+    },
+
+    {
+        id: 2,
+        nombre: "Camiseta Alternativa",
+        precio: 14800,
+        img:
+            "https://i.pinimg.com/originals/a3/99/89/a39989dfc651949dc479f860e48e225d.jpg",
+        cantidad: 1,
+    },
+    
+    
+    {
+        id: 3,
+        nombre: "Shoggin negro/bordo",
+        precio: 9999,
+        img:
+            "https://i.pinimg.com/originals/38/4d/c5/384dc5edef0585d681a5b1cefd070e36.jpg",
+        cantidad: 1,
+    },
+    
+    {
+        id: 4,
+        nombre: "Campera Roja/Negra",
+        precio: 22500,
+        img:
+            "https://i.pinimg.com/originals/e6/08/2f/e6082fbdb8d5e8429e6cc9ccaec1f8d8.jpg",
+        cantidad: 1,
+    },
+
+    
+];
